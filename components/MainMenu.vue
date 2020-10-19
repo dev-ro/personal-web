28c27f23a37084f15ba0b8ed3df3235e082d8a89
dev-ro <dev.rohit256@gmail.com>
2020-10-19 08:39:45 +0530

    changed menu

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -2,7 +2,7 @@
   <nav class="navbar shadow-sm">
       <div class="container d-flex justify-content-between align-items-center" :style="!showMenu ? 'padding-top: .8rem; padding-bottom: .8rem;' : ''">
            <Menu v-if="showMenu" class="d-flex flex-row" />
-            <font-awesome-icon @click="hamOpen=true" :icon="['fas' , 'bars']" class="text-white d-lg-none navbar-toggler-icon"></font-awesome-icon>
+            <font-awesome-icon @click="hamOpen=!hamOpen" :icon="['fas' , 'bars']" class="text-white d-lg-none navbar-toggler-icon"></font-awesome-icon>
            <SpotifyWidget />
       </div>
         <SideMenu
